c70d533d6c40dc5974ca58dc45d2476c16e05d94
Миллионеров <vdmozharov11@gmail.com>
2026-04-18 12:25:32 +0300

    Update ReportForm.h

--- ReportForm.h
+++ ReportForm.h
@@ -1,7 +1,8 @@
-﻿#pragma once
+#pragma once
 
 #using <System.Windows.Forms.dll>
 #using <System.Drawing.dll>
+#using <System.dll>
 
 #include "ExpenseManager.h"
 #include <msclr/marshal_cppstd.h>
@@ -16,10 +17,13 @@ namespace ExpenseTrackerApp {
         ExpenseManager* manager;
         ListView^ listViewReport;
         Button^ btnClose;
+        Button^ btnExportReport;
         Label^ labelTitle;
+        Label^ labelTotalInfo;
 
         void LoadReport();
         void btnClose_Click(Object^ sender, EventArgs^ e);
+        void btnExportReport_Click(Object^ sender, EventArgs^ e);
 
     public:
         ReportForm(ExpenseManager* mgr);
